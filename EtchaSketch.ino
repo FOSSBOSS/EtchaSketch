436637877939dcd10bd469b5c64d4f1c29c63084
FOSSBOSS <blicken@gmail.com>
2021-12-04 21:55:45 -0500

    Update EtchaSketch.ino
    replaced potentiates, for rotary encoders. got rid of html & css, using svg file format exclusively.

--- EtchaSketch.ino
+++ EtchaSketch.ino
@@ -1,4 +1,9 @@
+/*
+An IoT Etch-A-Sketch
+uses 2 rotory encoders to generate x,y data points
+in a svg poly line.
 
+*/
 #include <SPI.h>
 #include <NativeEthernet.h>
 #include <SD.h>
@@ -8,24 +13,20 @@ byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 //10.71.148.177 home 
-IPAddress ip(10, 71, 148, 177);
-//IPAddress ip(10, 1, 0, 177);
+//IPAddress ip(10, 71, 148, 177);
+IPAddress ip(10, 1, 0, 177);
 EthernetServer server(80);
 //Function defs
-void rotorSense(EthernetClient client);
+void rotors();
 void rotorA(EthernetClient client);
 void rotorB(EthernetClient client);
+
+void listenClient(EthernetClient client);
+void pageWrite(EthernetClient client);
+void polyLineBegin(EthernetClient client);
 void startPage(EthernetClient client);
 void endPage(EthernetClient client);
-void svgHeader(EthernetClient client);
-void svgFooter(EthernetClient client);
-void polyLineBegin(EthernetClient client);
-void polyLineEnd(EthernetClient client);
-void pageWrite(EthernetClient client);
-void listenClient(EthernetClient client);
-void background(EthernetClient client); //only want to do this one once, maybe I can run an Iframe or something
-void sensors();//pots
-/**Pot Vars**/ 
+
 int potX = A0;    
 int potY = A1;    
  
@@ -36,7 +37,7 @@ int oldX = 0;
 int oldY = 0;
 
 /**Rotor Vars**/
-// Rotary Encoder Input efintions
+// Rotary Encoder Input defintions
 #define CLKA 2
 #define DTA 3
 #define SWA 4
@@ -56,15 +57,10 @@ int BlastStateCLK;
 
 String AcurrentDir ="";  //just easier than cleverly combining this string for seprate IO
 String BcurrentDir ="";
-
-unsigned long AlastButtonPress = 0;
-unsigned long BlastButtonPress = 0;
-//
-
    
 void setup() {
 
-  delay(5000);  //you want this delay. reason: tl;dr
+  delay(10000);  //you want this delay. reason: tl;dr
   /** Set encoder pins as inputs **/
   pinMode(CLKA,INPUT);
   pinMode(DTA,INPUT);
@@ -101,8 +97,13 @@ if (Ethernet.linkStatus() == LinkOFF) {
 //if data file (on SD Card) Doesn't exist, create it. 
 //if data file (on SD Card) does exist errase it and create a new one
   if (SD.exists("datalog.txt")) {
-    Serial.println("datalog.txt exists: \n Removing it.");
-    SD.remove("datalog.txt");
+     Serial.println("datalog.txt exists: \n Removing it.");
+     while(SD.remove("datalog.txt")!=1){
+       Serial.println("Deleeting old file data");
+       if (SD.remove("datalog.txt")==1){
+          Serial.println ("old dataFile removed");
+       }
+     }   
   } else {
     Serial.println("datalog.txt doesn't exist.");
          }
@@ -121,9 +122,9 @@ if (Ethernet.linkStatus() == LinkOFF) {
 } //end setup
 
 void loop() {
-sensors();
  // listen for incoming clients
   EthernetClient client = server.available();
+rotors();
 listenClient(client);
 }  //end loop  
 void listenClient(EthernetClient client){
@@ -152,74 +153,30 @@ void listenClient(EthernetClient client){
     // close the connection:
     client.stop();
   } //end client
-  }
-void sensors(){
-    int sensorValX = analogRead(8);
-  int sensorValY = analogRead(9);
-  String dataString = "";
-  int threshhold =3;
-  int xVariance = abs(sensorValX-oldX);
-  int yVariance = abs(sensorValY-oldY);
-
-if (xVariance >=threshhold && yVariance >= threshhold ||xVariance >=threshhold || yVariance >=threshhold){
-//if sensor data is greater than threshold, write data to the sd card, then close the file
-    dataString += String(sensorValX);
-    dataString += ",";
-    dataString += String(sensorValY);
-
-   dataFile = SD.open("datalog.txt", FILE_WRITE);
-    // if the file is available, write to it:
-   if (dataFile) {
-      dataFile.println(dataString);
-      dataFile.close();
-      
-      //clear dataString
-      dataString="";
-  } else {
-    // if the file isn't open, pop up an error:
-    Serial.println("error opening datalog.txt");
-  }
-dataFile.close();
-  
-}else{
-  ;
-  }
-
-  }
+  }//end listener()
 
 void pageWrite(EthernetClient client){
           startPage(client);   
-          svgHeader(client);           
           polyLineBegin(client);
-          polyLineEnd(client);
-          svgFooter(client);       
           endPage(client);  
-  
   }
-
-
 void startPage(EthernetClient client){
-     // send a standard http response header
+     // doctype svg no need for html
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println("Refresh: 1");  // refresh the page automatically every 5 sec
           client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>"); 
-  }
-
+          client.println("<!DOCTYPE svg>");
+          client.println("<svg   xmlns='http://www.w3.org/2000/svg'  viewBox='0 0 227.35277 156.29323' height='6.1532764in' width='8.9508963in'><path  style='fill:#ff0000; stroke:#ff0000; stroke-width:1.32300019;stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1' d='m 12.000819,0.66145838 c -6.2819637,0 -11.33936062,5.05739692 -11.33936062,11.33936062 V 144.29249 c 0,6.28197 5.05739692,11.33936 11.33936062,11.33936 H 215.35224 c 6.28197,0 11.33885,-5.05739 11.33885,-11.33936 V 12.000819 c 0,-6.2819637 -5.05688,-11.33936062 -11.33885,-11.33936062 z M 18.932696,12.696383 H 205.5854 c 5.02557,0 9.07128,4.04571 9.07128,9.071282 v 77.03923 c 0,5.025575 -4.04571,9.071285 -9.07128,9.071285 H 18.932696 c -5.025572,0 -9.0712819,-4.04571 -9.0712819,-9.071285 v -77.03923 c 0,-5.025572 4.0457099,-9.071282 9.0712819,-9.071282 z' /><ellipse style='fill:#ffffff;fill-opacity:1; stroke:#ff0000; stroke-width:1.32300007; stroke-miterlimit:4; stroke-dasharray:none; stroke-opacity:1' cx='21.16666'  cy='130.59085'  rx='10.583334' ry='9.0714283' /><ellipse  style='fill:#ffffff; fill-opacity:1; stroke:#ff0000; stroke-width:1.32300007; stroke-miterlimit:4; stroke-dasharray:none;stroke-opacity:1' cx='200.62982'   cy='131.57361' rx='10.583334' ry='9.0714283'/>");
+          client.println("\n<polyline points='15,15");          
+   }
  void endPage(EthernetClient client){
-  client.println("</body>");
-  client.println("</html>");
-  }
-
-  
+  client.print("' \nfill='none' stroke='#ff0000'/></svg>");
+   }
 void polyLineBegin(EthernetClient client){
-  client.print("<polyline points=\""); //x,y points go here
-  //read data from SD card
+//read data points from SD card
     File dataFile = SD.open("datalog.txt");
-
   // if the file is available, read it:
   if (dataFile) {
     while (dataFile.available()) {
@@ -229,130 +186,81 @@ void polyLineBegin(EthernetClient client){
     dataFile.close();
   }
 }
+
   
-void polyLineEnd(EthernetClient client){
-   client.println ("0, 0\" stroke=\"red\" fill=\"transparent\" stroke-width=\"5\"/>");  //a dirty hack to avoid dealing with 1 last trailing comma.
-  }
- 
-void svgHeader(EthernetClient client){
-client.println("<svg width=\"1023\" height=\"1023\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">"); 
-  } 
-  
-void svgFooter(EthernetClient client){
-client.println ("</svg>");
-  }
-
-void background(EthernetClient client){
-//  client.println();
-
-client.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"
-"<svg"
-"xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:cc=\"http://creativecommons.org/ns#\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\""
-"xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:sodipodi=\"http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd\""
-"id=\"svg8\" version=\"1.1\" viewBox=\"0 0 227.35277 156.29323\""
-"height=\"6.1532764in\""
-"width=\"8.9508963in\""
-"sodipodi:docname=\"etchasketchborder.svg\">"
-"<defs"
-"id=\"defs2\" />"
-"<path"
-"style=\"fill:#ff0000;stroke:#ff0000;stroke-width:1.32300019;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1\""
-"d=\"m 12.000819,0.66145838 c -6.2819637,0 -11.33936062,5.05739692 -11.33936062,11.33936062 V 144.29249 c 0,6.28197 5.05739692,11.33936 11.33936062,11.33936 H 215.35224 c 6.28197,0 11.33885,-5.05739 11.33885,-11.33936 V 12.000819 c 0,-6.2819637 -5.05688,-11.33936062 -11.33885,-11.33936062 z M 18.932696,12.696383 H 205.5854 c 5.02557,0 9.07128,4.04571 9.07128,9.071282 v 77.03923 c 0,5.025575 -4.04571,9.071285 -9.07128,9.071285 H 18.932696 c -5.025572,0 -9.0712819,-4.04571 -9.0712819,-9.071285 v -77.03923 c 0,-5.025572 4.0457099,-9.071282 9.0712819,-9.071282 z\""
-"/>"
-"<ellipse"
-"style=\"fill:#ffffff;fill-opacity:1;stroke:#ff0000;stroke-width:1.32300007;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1\""
-"cx=\"21.16666\""
-"cy=\"130.59085\""
-"rx=\"10.583334\""
-"ry=\"9.0714283\" />"
-"<ellipse"
-"style=\"fill:#ffffff;fill-opacity:1;stroke:#ff0000;stroke-width:1.32300007;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1\""
-"cx=\"200.62982\""
-"cy=\"131.57361\""
-"rx=\"10.583334\""
-"ry=\"9.0714283\" />"
-"</svg>");
-  }  
-
- void rotorA(EthernetClient client){
- // Read the current state of CLK
+int rotorA(){ //X coords
   AcurrentStateCLK = digitalRead(CLKA);
-
-  // If last and current state of CLK are different, then pulse occurred
-  // React to only 1 state change to avoid double count
   if (AcurrentStateCLK != AlastStateCLK  && AcurrentStateCLK == 1){
-
-    // If the DT state is different than the CLK state then
-    // the encoder is rotating CCW so decrement
-    if (digitalRead(DTA) != AcurrentStateCLK) {
+     if (digitalRead(DTA) != AcurrentStateCLK) {
       Acounter --;
-      AcurrentDir ="CCW";
-    } else {
-      // Encoder is rotating CW so increment
-      Acounter ++;
-      AcurrentDir ="CW";
-    }
-    Serial.print("A Direction: ");
-    Serial.print(AcurrentDir);
-    Serial.print(" | Counter: ");
-    Serial.println(Acounter);
-  }
-  int AbtnState = digitalRead(SWA);
-
-  //If we detect LOW signal, button is pressed
-  if (AbtnState == LOW) {
-    //if 50ms have passed since last LOW pulse, it means that the
-    //button has been pressed, released and pressed again
-    if (millis() - AlastButtonPress > 500) {
-      Serial.println("Button A pressed!");
-    }
-  AlastButtonPress = millis();
-  }
-  // Remember last CLK state
+      }else{
+       Acounter ++;
+       }
+     }
+// Remember last CLK state
   AlastStateCLK = AcurrentStateCLK;
-//endA
-
-
+//X limits
+if(Acounter<=15){
+  Acounter=15;
   }
-void rotorB(EthernetClient client){
+if(Acounter>=215){
+  Acounter=215;
+  }  
+return Acounter;
+  }//endA rotor
   
+int rotorB(){ //Y coords
   BcurrentStateCLK = digitalRead(CLKB);
-  // If last and current state of CLK are different, then pulse occurred
-  // React to only 1 state change to avoid double count
   if (BcurrentStateCLK != BlastStateCLK  && BcurrentStateCLK == 1){
-
-    // If the DT state is different than the CLK state then
-    // the encoder is rotating CCW so decrement
     if (digitalRead(DTB) != BcurrentStateCLK) {
-      Bcounter --;
-      BcurrentDir ="CCW";
-    } else {
-      // Encoder is rotating CW so increment
-      Bcounter ++;
-      BcurrentDir ="CW";
-    }
-
-    Serial.print("B Direction: ");
-    Serial.print(BcurrentDir);
-    Serial.print(" | Counter: ");
-    Serial.println(Bcounter);
+       Bcounter --;
+       }else{
+        Bcounter ++;
+     }
+    //Serial.println(Bcounter);
   }  
   BlastStateCLK = BcurrentStateCLK;
-  // Read the button state
-  int BbtnState = digitalRead(SWB);
-    if (BbtnState == LOW) {
-    //if 50ms have passed since last LOW pulse, it means that the
-    //button has been pressed, released and pressed again
-    if (millis() - BlastButtonPress > 50) {
-      Serial.println("Button B pressed!");
-    }
+//Y limits
+if(Bcounter<=15){
+  Bcounter=15;
+  }
+if(Bcounter>=210){
+  Bcounter=210;
+  }  
+  return Bcounter;
+  }//EndB rotor
+
+void rotors(){
+//read rotors and write x,y points to datalog.txt  
+ sensorValX=rotorA();
+ sensorValY=rotorB();
+String dataString = "";
+
+if(sensorValX!=oldX || sensorValY!=oldY){
+   dataString += ",";
+   dataString += String(sensorValX);
+   dataString += ", ";
+   dataString += String(sensorValY);
+   
+   Serial.println(dataString);
+   
+   dataFile = SD.open("datalog.txt", FILE_WRITE);
+    // if the file is available, write to it:
+   if (dataFile) {
+      dataFile.print(dataString);
+      dataFile.close();
+      //clear dataString
+      dataString="";
+      }else{
+    // if the file isn't open, pop up an error:
+    Serial.println("error opening datalog.txt");
+  }
+  dataFile.close();
+  }else{
+  ;
+  }
+
+ oldX=sensorValX;
+ oldY=sensorValY;
   
- // Remember last button press event
-    BlastButtonPress = millis();
-  }
-//endB
-}
-void rotorSense(EthernetClient client){
-  rotorA(client);
-  rotorB(client);
-  }
+}//end rotors 
